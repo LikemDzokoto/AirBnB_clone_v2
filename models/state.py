c4261d25616d0aa1ebd14a7f89fbdbc6b1235062
LikemDzokoto <likemdzokoto@gmail.com>
2022-10-12 22:17:11 -0700

    test and update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column,String
+from sqlalchemy.orm import relationship 
+import models 
+from os import getenv  
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    if getenv("HBNB_TYPE_STORAGE") =="db": 
+        name = Column(String(128),nullabe=False)
+        cities = relationship("Cities", backref="state", cascade="all,delete,delete-orphan")
+    else:
+        name =""
+
+    @property
+    def cities(self):
+        state_list =[]
+        dic = models.storage.all("City")
+        for city in dic.items():
+            if city.state_id == self.id:
+                state_list.append(city)
+        return state_list 
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
+import unittest
+from models.base_model import BaseModel
 from models.state import State
+from os import getenv, remove 
+import pep8
+
+storage = getenv("HBNB_TYPE_STORAGE", "fs")
+
+class TestState(unittest.TestCase):
+    """Test the State class. """
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Sets up unittest
+        '''
+        cls.new_state = State()
+        cls.new_state.name = "California"
 
 
-class test_state(test_basemodel):
-    """ """
 
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):        
         """ """
         super().__init__(*args, **kwargs)
         self.name = "State"
@@ -17,3 +32,12 @@ class test_state(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+    @unittest.skipIf(storage == "db", "Testing database storage only")
+    def test_State_attributes_type(self):
+        '''
+            Test that State class attribute name is class type str.
+        '''
+        name = getattr(self.new_state, "name")
+        self.assertIsInstance(name, str)
